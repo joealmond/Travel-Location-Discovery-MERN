92a246d46dd158723c7e2ecbf71a46e5345a1df9
joealmond <jozsef.mandula@gmail.com>
2023-06-14 23:28:31 +0200

    refractored VisitedPoiList

--- client/src/components/VisitedPoisList.jsx
+++ client/src/components/VisitedPoisList.jsx
@@ -1,38 +1,34 @@
-
-import VisitedLocationCard from "./VisitedLocationCard";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import VisitedLocationCard from "./VisitedLocationCard";
 
-const fetchVisitedPois = () => {
-    return fetch("http://localhost:3000/visited").then((res) => res.json())
+function VisitedPoisList() {
+  const navigate = useNavigate();
+  const [visitedPois, setVisitedPois] = useState([]);
+
+  const fetchVisitedPois = () => {
+    return fetch("http://localhost:3000/visited")
+      .then((res) => res.json())
+      .then((pois) => {
+        setVisitedPois(pois);
+      });
+  };
+
+  useEffect(() => {
+    fetchVisitedPois();
+  }, []);
+
+  return (
+    <>
+      <button onClick={() => navigate("/")}>Main Page</button>
+      {visitedPois && (
+        <VisitedLocationCard
+          visitedDB={visitedPois}
+          onDelete={() => fetchVisitedPois()}
+        />
+      )}
+    </>
+  );
 }
 
-function VisitedPoisList () {
-const navigate = useNavigate()
-
-    const [visitedPois, setVisitedPois] = useState([])
-
-    function fetchRefreshedVisitedPois() {
-        fetchVisitedPois()
-            .then((pois) => {
-                setVisitedPois(pois);
-            })
-    }
-
-    useEffect(() => {
-        fetchVisitedPois()
-            .then((pois) => 
-            setVisitedPois(pois)
-            )
-    }, [])
-
-    return (<>
-    <button onClick={()=>navigate('/')}>Main Page</button> 
-    {visitedPois && <VisitedLocationCard
-             visitedDB={visitedPois}
-             onDelete={fetchRefreshedVisitedPois}
-            />}
-            </>)
-}
-
-export default VisitedPoisList
+export default VisitedPoisList;
